5adc972514729c6b0d00ea77dc2f5f4cbe8c0ea7
Sava-B <savab2164@gmail.com>
2023-12-13 20:04:02 -0700

    last edit for a while

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+# Did not work... need to pay for openai tokens... putting projects on pause
+
 from openai import OpenAI
 
 client = OpenAI()
 
-audio_file = open("/path/to/file/audio.mp3", "rb")
-transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
